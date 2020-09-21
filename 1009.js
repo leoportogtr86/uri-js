43a6072739a194892326afe7cd1da93ba645ba84
Leo Porto <leoporto_studio@hotmail.com>
2020-09-20 21:19:53 -0300

    1009

---
+++ 1009.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('./dev/stdin/file.txt', 'utf-8')
+var lines = input.split('\n')
+
+let nome = lines.shift()
+let salario_fixo = lines.shift()
+let total_vendas = lines.shift()
+let comissao = (15 / 100) * Number(total_vendas)
+let total = Number(salario_fixo) + Number(comissao)
+
+console.log("TOTAL = R$ " + total.toFixed(2));
